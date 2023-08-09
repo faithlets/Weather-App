b11aaad62b60ebb49258fbab6277cb02a2daad68
faithlets <faithletsoara@gmail.com>
2023-08-09 02:33:54 +0100

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 function formatDate(timestamp) {
+  console.log(response.data);
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
@@ -21,16 +22,23 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  descriptionElementElement.innerHTML = response.data.weather(0).description;
-  humidityElementElement.innerHTML = response.data.main.humidity;
+  descriptionElement.innerHTML = response.data.weather(0).description;
+  humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElementElement.innerHTML = formatDate(response.data.dt * 1000);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "9a34f9123f26fa9de7f9cfdc91eeea0b";
+let city = "Cape Town";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cape Town&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 
