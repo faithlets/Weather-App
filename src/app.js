4a807f6079a1ad4647c1ea3449091d9c414d9699
faithlets <faithletsoara@gmail.com>
2023-08-09 01:18:27 +0100

    Added app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = date.getDay();
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +21,14 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#speed");
+  let dateElement = document.querySelector("#date");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElementElement.innerHTML = response.data.weather(0).description;
   humidityElementElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElementElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "9a34f9123f26fa9de7f9cfdc91eeea0b";
